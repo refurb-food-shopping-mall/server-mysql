2ed2e1636978eb02136dc3dff48af4d7216b9df8
dinggu <zaqxsw9705@naver.com>
2021-06-26 01:49:04 +0900

    💄 사용자의 기본배송지 정보 요청을 받는 라우터 작성

--- routes/address.js
+++ routes/address.js
@@ -4,6 +4,26 @@ const  initModels  =  require ( "../models/init-models" );
 
 const  models  =  initModels ( sequelize );
 
+router.post('/address/default', async (req, res) => {
+    try{
+        let useraddress = await models.t_address.findAll({ 
+            where : { 
+                id : req.body.userid,
+                default_address : req.body.default_address
+            }   
+        })
+        res.json({
+            success : true,
+            useraddress
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }    
+})
+
 
 
 module.exports = router
