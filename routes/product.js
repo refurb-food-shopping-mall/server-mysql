e46e3966f842fe6e24e789c38bf542613d58f216
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2021-06-26 12:01:17 +0900

    💄 상품 정보 전체조회 api, 단일 상품 등록 api 생성

--- routes/product.js
+++ routes/product.js
@@ -1,36 +1,71 @@
 const router = require('express').Router()
 //const verifyToken = require('../middlewares/verify-token')
 const { sequelize } = require('../models');
-const  initModels  =  require ( "../models/init-models" );
+const initModels = require("../models/init-models");
 const t_product_image = require('../models/t_product_image');
 
-const  models  =  initModels ( sequelize );
+const models = initModels(sequelize);
 
 // [[ 상품 정보와 썸네일이미지 조회 ]]
 router.post('/product/thumnail', async (req, res) => {
     let res_array = [];
-    for(let i = 0 ; i < req.body.productarray.length ; i++){
-        let product =  await models.t_product.findAll({
-                            where : {
-                                id : req.body.productarray[i]
-                            },
-                            include: [{
-                                as : 't_product_images',
-                                model: models.t_product_image,
-                                attributes: ['path'],
-                                where: {
-                                    product_id : req.body.productarray[i],
-                                    type_image : 1
-                                }
-                            }]
-                        })
+    for (let i = 0; i < req.body.productarray.length; i++) {
+        let product = await models.t_product.findAll({
+            where: {
+                id: req.body.productarray[i]
+            },
+            include: [{
+                as: 't_product_images',
+                model: models.t_product_image,
+                attributes: ['path'],
+                where: {
+                    product_id: req.body.productarray[i],
+                    type_image: 1
+                }
+            }]
+        })
         //console.log(product[0].dataValues);
         //console.log(product[0].dataValues.t_product_images[0].dataValues);
         res_array.push(product[0].dataValues);
     }
     //console.log(res_array);
-    res.send(res_array);   
+    res.send(res_array);
 });
 
+// API - get All Products 
+router.get('/product', async (req, res) => {
+    try {
+        const allProduct = await models.t_product.findAll()
+        res.status(201).json(allProduct)
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+// API - post single product 
+router.post('/product', async (req, res) => {
+    try {
+        const newProduct = await models.t_product.create({
+            seller_id: req.body.sellerID,
+            product_category: req.body.productCategory,
+            product_name: req.body.productName,
+            product_price: req.body.productPrice,
+            delivery_price: req.body.productDeliveryPrice,
+            add_delivery_price: req.body.addDeliveryPrice,
+            total_product: req.body.totalProduct,
+            average_star_grade: req.body.averageStarGrade,
+            acknowledged_product: req.body.acknowledgedProduct,
+        })
+        res.status(201).json(newProduct)
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
 
 module.exports = router
