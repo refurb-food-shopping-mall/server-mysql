18d44bf41b528bf962d317406c97e61529168a8a
ezn6 <kcourtney@naver.com>
2021-07-05 21:09:05 +0900

    💄 api 모두 try catch로 감싸도록 수정하고, 요청한 기간을 부르는 쿼리 만듦

--- routes/pdetail.js
+++ routes/pdetail.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { sequelize } = require('../models');
 const initModels = require("../models/init-models");
 const models = initModels(sequelize);
+const { Op } = require('sequelize');
 
 
 router.post('/getProduct', async (req, res) => {
@@ -25,47 +26,78 @@ router.post('/getProduct', async (req, res) => {
 
 
 router.post('/getOrder', async (req, res) => {
-    const getorder = await models.t_order.findAll({
-        where: {
-            id: req.body.id
-        }
-    })
-    res.json({
-        success: true,
-        getorder,
-    })
+    try {
+        const getorder = await models.t_order.findAll({
+            where: {
+                id: req.body.id
+            }
+        })
+        res.json({
+            success: true,
+            getorder,
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
 })
 
 
 router.post('/getAddress', async (req, res) => {
-    const getaddress = await models.t_address.findAll({
-        where: {
-            id: req.body.addressid
-        }
-    })
-    res.json({
-        success: true,
-        getaddress,
-    })
+    try {
+        const getaddress = await models.t_address.findAll({
+            where: {
+                id: req.body.addressid
+            }
+        })
+        res.json({
+            success: true,
+            getaddress,
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
 })
 
 
-router.post('/getget', async (req, res) => {
-    const getgett = await models.t_order.findAll({
+router.post('/getDate', async (req, res) => {
+    try {
+        let startDate = req.body.dayarr[0]
+        let endDate = req.body.dayarr[1]
+        const getgett = await models.t_order.findAll({
+            where: {
+                // expected_del_day: req.body.dayarr[i](i=0,1)
+                // expected_del_day: { gte: req.body.dayarr[0] },
+                // expected_del_day: { lte: req.body.dayarr[1] }
 
-        where: {
-            // expected_del_day: req.body.dayarr[i](i=0,1)
-            // expected_del_day: { gte: req.body.dayarr[0] },
-            // expected_del_day: { lte: req.body.dayarr[1] }
-            expected_del_day: req.body.dayarr[0]
-            //id: req.body.dayarr
-        }
-    })
-    console.log(getgett)
-    res.json({
-        success: true,
-        getgett,
-    })
+                [Op.or]: [{
+                    expected_del_day: {
+                        [Op.between]: [startDate, endDate]
+                    }
+                }, {
+                    expected_del_day: {
+                        [Op.between]: [startDate, endDate]
+                    }
+                }]
+
+            }
+        })
+        //console.log(getgett)
+        res.json({
+            success: true,
+            getgett,
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
 })
 
 
