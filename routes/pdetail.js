3cdeeccb263180bd2d6cd67011ca3e19665b06a6
ezn6 <kcourtney@naver.com>
2021-07-16 17:32:28 +0900

    1.상품정보와 해당 사진 함께 가져오기 2. shipping페이지 상단에 쓰여질 배송상태 숫자 카운팅할수 있게 배열 추가함

--- routes/pdetail.js
+++ routes/pdetail.js
@@ -10,7 +10,15 @@ router.post('/getProduct', async (req, res) => {
         const getproduct = await models.t_product.findAll({
             where: {
                 id: req.body.productid
-            }
+            },
+            include: [{
+                as: 't_product_images',
+                model: models.t_product_image,
+                attributes: ['path'],
+                where: {
+                    type_image: 1
+                }
+            }]
         })
         res.json({
             success: true,
@@ -68,6 +76,10 @@ router.post('/getAddress', async (req, res) => {
 
 router.post('/getDate', async (req, res) => {
     try {
+        let cnt1 = 0
+        let cnt2 = 0
+        let cnt3 = 0
+        let cntarr = []
         let arr = []
         let startDate = req.body.dayarr[0]
         let endDate = req.body.dayarr[1]
@@ -78,27 +90,44 @@ router.post('/getDate', async (req, res) => {
                 },
                 user_id: req.body.user_id,
             },
-            attributes: ['id', 'product_amount', 'order_status', 'ordered_day', 'product_id']
+            attributes: ['id', 'product_amount', 'order_status', 'ordered_day', 'product_id'],
+            order: [['ordered_day', 'DESC']]
         })
             .then(async (order) => {
                 //console.log(order)
                 for (let i = 0; i < order.length; i++) {
+                    if (order[i].dataValues.order_status == '배송준비중') { cnt1 += 1 }
+                    else if (order[i].dataValues.order_status == '배송중') { cnt2 += 1 }
+                    else if (order[i].dataValues.order_status == '반품중') { cnt3 += 1 }
+
                     let data = await models.t_product.findAll({
                         where: {
                             id: order[i].dataValues.product_id
                         },
-                        attributes: ['product_name', 'product_price', 'delivery_price', 'add_delivery_price']
+                        include: [{
+                            as: 't_product_images',
+                            model: models.t_product_image,
+                            attributes: ['path'],
+                            where: {
+                                type_image: 1
+                            }
+                        }],
+                        //attributes: ['product_name', 'product_price', 'delivery_price', 'add_delivery_price', 't_product_images'],
                     })
                     //console.log(data)
                     order[i].dataValues.key = data[0].dataValues
                     arr.push(order[i].dataValues)
                 }
-                //console.log(arr)
+                cntarr.push(cnt1)
+                cntarr.push(cnt2)
+                cntarr.push(cnt3)
+                //console.log(cntarr)
             })
 
         res.json({
             success: true,
             arr,
+            cntarr
         })
     } catch (err) {
         res.status(500).json({
