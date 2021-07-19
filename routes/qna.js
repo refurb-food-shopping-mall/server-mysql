59e21394ebe67821951317cffe474d4bef0ba11f
Sw Lee <studentinbb@gmail.com>
2021-07-19 20:36:30 +0900

    qna 등록 라우터 수정: 등록 후 해당 페이지 qna 다시 가져오도록 라우터 수정

--- routes/qna.js
+++ routes/qna.js
@@ -54,18 +54,30 @@ router.get('/readqna/:id', async (req, res) => {
 
 router.post('/qna/save', async (req, res) => {
     try {
-        let SavaQna = await models.t_question.create({
+        models.t_question.create({
             q_title : req.body.q_title,
             q_description : req.body.q_description,
             user_id : req.body.user_id,
             product_id : req.body.product_id,
             q_type: 1,
-            answer_status: 1
-        });
-        console.log(SavaQna.dataValues.id);
-        res.json({
-            success: true,
-            qna_id: SaveQna.dataValues.id
+            answer_status: 1,
+            answer_description: ""
+        })
+        .then((qna) => {
+            return models.t_question.findAll({
+                where: {product_id : req.body.product_id}
+                ,include: [{
+                    as: 'user',
+                    model: models.t_user,
+                    attributes: ['user_name']
+                }]
+            })
+            .then(result => {
+                res.json({
+                    success: true,
+                    result
+                })
+            })
         })
     } catch (err) {
         res.status(500).json({
@@ -75,7 +87,7 @@ router.post('/qna/save', async (req, res) => {
     }
 });
 
-router.get('/dltjddn', verifyToken, (req, res) => {
+router.get('/verify_user', verifyToken, (req, res) => {
     try {
       const { user_name, id } = req.decode
       res.json({
